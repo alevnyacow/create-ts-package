75ecfb0f52e0b62fc025d09d8cdd1ee7f6fde892
alevnyacow <alevnyacow@gmail.com>
2022-02-20 11:58:47 +0300

    Fixed format script

--- root-level-files/index.ts
+++
@@ -1,6 +0,0 @@
-import { join } from "path";
-import { readDirectoryFiles } from "@alevnyacow/get-directory-files";
-
-const rootFilesContent = readDirectoryFiles(join(__dirname, "data"));
-
-export { rootFilesContent };
