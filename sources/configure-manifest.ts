57987183030f5d45a615851c78ab8adf601e8c14
alevnyacow <alevnyacow@gmail.com>
2022-02-25 18:13:10 +0300

    refactored configuration (still compiled though maybe it's not so critical in case of npx script)

--- sources/configure-manifest.ts
+++ sources/configure-manifest.ts
@@ -1,12 +1,24 @@
 import { writeFileSync, readFileSync } from "fs";
-import { scripts } from "./configuration";
+
+/**
+ * Package scripts data.
+ */
+const packageScripts = {
+    test: "jest",
+    // eslint-disable-next-line camelcase
+    clean_transpiled: "del-cli --force ./transpiled",
+    format: "prettier --config .prettierrc --write sources/**",
+    lint: "eslint sources/**/*.ts --ext .ts",
+    check: "npm run lint && npm run test",
+    build: "npm run clean_transpiled && tsc",
+    execute: "npm run build && node transpiled/index.js"
+};
 
 /**
  * Modifies package manifest with scripts described in configuration
  * and sets main and types sections for the package.
  */
 export const configureManifest = (packageName: string) => {
-    const packageScripts = scripts();
     const packageManifestContent = readFileSync(
         `./${packageName}/package.json`
     );
--- sources/create-root-files.ts
+++ sources/create-root-files.ts
@@ -1,10 +1,13 @@
-import { rootFilesContent } from "./configuration";
+import { join } from "path";
 import { writeFileSync } from "fs";
-
+import { readDirectoryFiles } from "@alevnyacow/get-directory-files";
 /**
  * Creates root files for package configuration.
  */
 export const createRootFiles = (packageName: string) => {
+    const rootFilesContent = readDirectoryFiles(
+        join(__dirname, "root-level-files")
+    );
     Object.keys(rootFilesContent).forEach((fileName) => {
         writeFileSync(
             `./${packageName}/${fileName}`,
